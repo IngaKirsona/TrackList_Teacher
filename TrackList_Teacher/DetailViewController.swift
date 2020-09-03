b5fff6c25824fbc1c28b7a8d1fed236b5879d24a
IngaKirsona <ingas@inbox.lv>
2020-09-03 08:36:17 +0300

    first commit 23:19

---
+++ TrackList_Teacher/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  TrackList_Teacher
+//
+//  Created by Inga Kirsona on 03/09/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
